cab18953fcaba0474447fb059e0c9a0c1d3a5e06
Karthik-Korada <147791155+Karthik-Korada@users.noreply.github.com>
2024-01-08 12:26:49 +0530

    Update my app.py

--- my app.py
+++ my app.py
@@ -1,11 +1,10 @@
 import streamlit as st
 st.set_page_config(page_title='Cats')
 st.header("kings of Ap")
-
 col1, col2 = st.columns(2)
 with col1:
   st.subheader("2 Cat")
-  st.image("./2.jpg", caption="2 ", width=300,use_column_width=True)
+  st.image("./2.webp", caption="2 ", width=300,use_column_width=True)
   st.write("Persian cats are cute")
 with col2:
   st.subheader("7 ")
